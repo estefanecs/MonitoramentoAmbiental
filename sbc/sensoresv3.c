25b1188a2ff56ad1f75da428ab936e1793e149bb
JoaoVitorSantDrade <jvitorsantdrade@gmail.com>
2022-06-08 07:54:58 -0300

    Atualização na parte do sensor DHT11

--- sbc/sensoresv3.c
+++ sbc/sensoresv3.c
@@ -113,66 +113,73 @@ int main( void ){
 
 // Funcao que realiza a leitura do sensor DHT11
 void leitura_dht11(char *temperatura, char *umidade){
-	char umidade2[10]; //armazena a segunda parte do valor de umidade
-	char temperatura2[10]; //armazena a segunda parte do valor de temperatura
-	
-	uint8_t laststate = HIGH;
-	uint8_t counter	= 0;
-	uint8_t j = 0, i;
-	float f; 
- 
-	dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
 
-	wiringPiSetup(); 
-	pinMode( DHTPIN, OUTPUT );
-	digitalWrite( DHTPIN, LOW );
-	delay( 20 );
-	digitalWrite( DHTPIN, HIGH );
-	delayMicroseconds( 50 );
-	pinMode( DHTPIN, INPUT );
- 
-	for ( i = 0; i < MAXTIMINGS; i++ ){
-		counter = 0;
-		while ( digitalRead( DHTPIN ) == laststate ){
-			counter++;
-			delayMicroseconds( 1 );
-			if ( counter == 255 ){
-				break;
-			}
-		}
-		laststate = digitalRead( DHTPIN );
- 
-		if ( counter == 255 )
-			break;
- 
-		if ( (i >= 4) && (i % 2 == 0) ){
-			dht11_dat[j / 8] <<= 1;
-			if ( counter > 16 ) // antes era 16
-				dht11_dat[j / 8] |= 1;
-			j++;
-		}
-	}
- 
-	if ( (j >= 40) &&
-	     (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
-	{
-		f = dht11_dat[2] * 9. / 5. + 32;
-	
-		//Armazenando o valor da umidade
-		sprintf(umidade, "%d.", dht11_dat[0]);
-		sprintf(umidade2, "%d", dht11_dat[1]);
-		strcat(umidade, umidade2);
-		
-		//Armazenando o valor da temperatura
-		sprintf(temperatura, "%d.", dht11_dat[2]);
-		sprintf(temperatura2, "%d", dht11_dat[3]);
-		strcat(temperatura, temperatura2);
-		
-		delay( 1000 ); 
+    int leu = 0;
+    
+    do{
+        char umidade2[10]; //armazena a segunda parte do valor de umidade
+        char temperatura2[10]; //armazena a segunda parte do valor de temperatura
+        
+        uint8_t laststate = HIGH;
+        uint8_t counter	= 0;
+        uint8_t j = 0, i;
+        float f; 
+    
+        dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
 
-	}else  {
-		//printf( "Data not good, skip\n" );
-	}
+        wiringPiSetup(); 
+        pinMode( DHTPIN, OUTPUT );
+        digitalWrite( DHTPIN, LOW );
+        delay( 20 );
+        digitalWrite( DHTPIN, HIGH );
+        delayMicroseconds( 50 );
+        pinMode( DHTPIN, INPUT );
+    
+        for ( i = 0; i < MAXTIMINGS; i++ ){
+            counter = 0;
+            while ( digitalRead( DHTPIN ) == laststate ){
+                counter++;
+                delayMicroseconds( 1 );
+                if ( counter == 255 ){
+                    break;
+                }
+            }
+            laststate = digitalRead( DHTPIN );
+    
+            if ( counter == 255 )
+                break;
+    
+            if ( (i >= 4) && (i % 2 == 0) ){
+                dht11_dat[j / 8] <<= 1;
+                if ( counter > 16 ) // antes era 16
+                    dht11_dat[j / 8] |= 1;
+                j++;
+            }
+        }
+    
+        if ( (j >= 40) &&
+            (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
+        {
+            //f = dht11_dat[2] * 9. / 5. + 32;
+        
+            //Armazenando o valor da umidade
+            sprintf(umidade, "%d.", dht11_dat[0]);
+            sprintf(umidade2, "%d", dht11_dat[1]);
+            strcat(umidade, umidade2);
+            
+            //Armazenando o valor da temperatura
+            sprintf(temperatura, "%d.", dht11_dat[2]);
+            sprintf(temperatura2, "%d", dht11_dat[3]);
+            strcat(temperatura, temperatura2);
+            
+            leu = 1;
+
+        }else  {
+            leu = 0;
+            delay( 1000 ); 
+        }
+    }while(leu == 0);
+	
 }
 
 // Funcao para simular o sensor de luminosidade BH1750
