7c47c646ce9c0a61af034a9d319bf6da69db842a
JoaoVitorSantDrade <jvitorsantdrade@gmail.com>
2022-06-18 12:47:37 -0300

    Adicionado codigo do DHT11 e seu Header

---
+++ sbc_atualizado/dht11.c
@@ -0,0 +1,57 @@
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "dht11.h"
+
+int * leitura_dht11(){
+
+    int dht11_dat[5] = {0};
+
+    do{  
+        uint8_t laststate = HIGH;
+        uint8_t counter = 0;
+        uint8_t j = 0, i;
+        float f; 
+    
+        dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+
+        wiringPiSetup(); 
+        pinMode( DHTPIN, OUTPUT );
+        digitalWrite( DHTPIN, LOW );
+        delay( 20 );
+        digitalWrite( DHTPIN, HIGH );
+        delayMicroseconds( 50 );
+        pinMode( DHTPIN, INPUT );
+    
+        for ( i = 0; i < MAXTIMINGS; i++ ){
+            counter = 0;
+            while ( digitalRead( DHTPIN ) == laststate ){
+                counter++;
+                delayMicroseconds( 1 );
+                if ( counter == 255 ){
+                    break;
+                }
+            }
+            laststate = digitalRead( DHTPIN );
+    
+            if ( counter == 255 )
+                break;
+    
+            if ( (i >= 4) && (i % 2 == 0) ){
+                dht11_dat[j / 8] <<= 1;
+                if ( counter > 16 )
+                    dht11_dat[j / 8] |= 1;
+                j++;
+            }
+        }
+    
+        if ( (j >= 40) &&
+            (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
+        {
+            return dht11_dat;
+        }
+    }while(1);
+    
+}
---
+++ sbc_atualizado/dht11.h
@@ -0,0 +1,6 @@
+#ifndef _dht11_h
+#define _dht11_h
+
+int * leitura_dht11();
+
+#endif
