9ee5f358f87725463531cc53f2cf70c46ac94ce9
Estéfane Souza <53922973+estefanecs@users.noreply.github.com>
2022-06-21 09:55:48 -0300

    Update sbchistorico.c

--- 20_06/sbchistorico.c
+++ 20_06/sbchistorico.c
@@ -32,7 +32,7 @@
 unsigned int tempo =0;
 unsigned int tempoAnterior=0;
 char tempoLido[]="";
-float intervaloTempo=2.0;
+global float intervaloTempo=2.0;
 char temperatura[10]={"0"};
 char umidade[10]={"0"}; 
 int  pressao=0,luminosidade=0;
